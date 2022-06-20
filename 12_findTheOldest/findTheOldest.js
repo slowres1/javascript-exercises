a030d335e656ef7b81879fe392f979b749646ed0
Stanley <slowres@gmail.com>
2022-06-20 23:47:17 +0100

    completed 11

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldest = people.sort(function(a, b) {
+        const last = getAge(a.yearOfBirth, a.yearOfDeath);
+        const next = getAge(b.yearOfBirth, b.yearOfDeath);
+        return last > next ? -1 : 1;
+    });
+    return oldest[0];
 };
+
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
